0f50f12583217e2f499e91d22429558af1ef653a
thaim <thaim24@gmail.com>
2021-07-31 12:41:30 +0000

    add sample implementation to show terraform cloud orgs

--- cmd_pull.go
+++ cmd_pull.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
+	tfe "github.com/hashicorp/go-tfe"
 	"github.com/urfave/cli/v2"
 )
 
 func pull(c *cli.Context) error {
+	ctx := context.Background()
 	log.Println("pull command")
+
+	config := &tfe.Config{
+		Token: c.String("tfetoken"),
+	}
+	tfeClient, err := tfe.NewClient(config)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	orgs, err := tfeClient.Organizations.List(ctx, tfe.OrganizationListOptions{})
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	for _, o := range orgs.Items {
+		fmt.Println("Name: " + o.Name)
+		fmt.Println("Email: " + o.Email)
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/thaim/terravars
 
 go 1.16
 
-require github.com/urfave/cli/v2 v2.3.0
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.0 // indirect
+	github.com/hashicorp/go-tfe v0.17.1 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
