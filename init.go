523967bc4d5091a1611e69266a89eebf990eb2eb
thaim <thaim24@gmail.com>
2023-02-25 15:54:57 +0900

    replace default logger with zerolog

--- init.go
+++ init.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/antonholmquist/jason"
 	tfe "github.com/hashicorp/go-tfe"
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
@@ -16,25 +16,25 @@ func NewTfeClient(c *cli.Context) (*tfe.Client, error) {
 	if token == "" {
 		home, err := os.UserHomeDir()
 		if err != nil {
-			log.Fatal(err)
+			log.Error().Err(err).Msg("failed to get user home directory")
 			return nil, err
 		}
 		fp, err := os.Open(filepath.Join(home, ".terraform.d/credentials.tfrc.json"))
 		if err != nil {
-			log.Fatal(err)
+			log.Error().Err(err).Msg("cannot find terraform cloud credential file")
 			return nil, err
 		}
 		defer fp.Close()
 
 		v, err := jason.NewObjectFromReader(fp)
 		if err != nil {
-			log.Fatal(err)
+			log.Error().Err(err).Msg("cannot read from terraform cloud credential file")
 			return nil, err
 		}
 
 		token, err = v.GetString("credentials", "app.terraform.io", "token")
 		if err != nil {
-			log.Fatal(err)
+			log.Error().Err(err).Msg("cannot retrieve credentails from terraform cloud credential file")
 			return nil, err
 		}
 	}
@@ -44,7 +44,7 @@ func NewTfeClient(c *cli.Context) (*tfe.Client, error) {
 	}
 	tfeClient, err := tfe.NewClient(config)
 	if err != nil {
-		log.Fatal(err)
+		log.Error().Err(err).Msg("cannot create tfe client")
 		return nil, err
 	}
 
