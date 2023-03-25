a5e20a006041cc6798c429714ba4bf4af1559503
thaim <thaim24@gmail.com>
2023-03-25 21:10:03 +0900

    add parser to show backend config

--- terraform_cloud.go
+++ terraform_cloud.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/rs/zerolog/log"
+)
+
 func updateTerraformCloudWorkspace(organization string, workspaceName string) (string, string) {
+	p := hclparse.NewParser()
+	file, diags := p.ParseHCLFile("main.tf")
+	if diags.HasErrors() {
+		log.Warn().Msgf("failed to parse main.tf to retrive organization and workspace %s", diags.Error())
+		return organization, workspaceName
+	}
+
+	fmt.Printf("%s\n", file.Bytes)
+
 	return organization, workspaceName
 }
