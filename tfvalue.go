fbcc36a231df9bab5414607c80a02602b2240426
thaim <thaim24@gmail.com>
2023-04-05 21:26:21 +0900

    impliment cty.Value to String for number, bool and tuple

--- tfvalue.go
+++ tfvalue.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -9,6 +11,23 @@ func String(value cty.Value) string {
 	switch ty {
 	case cty.String:
 		return value.AsString()
+	case cty.Number:
+		return value.AsBigFloat().String()
+	case cty.Bool:
+		return strconv.FormatBool(value.True())
+	}
+
+	if ty.IsTupleType() {
+		valString := "["
+		for idx, elm := range value.AsValueSlice() {
+			if idx != 0 {
+				valString += ", "
+			}
+			valString += `"` + String(elm) + `"`
+		}
+		valString += "]"
+
+		return valString
 	}
 
 	return ""
--- tfvalue_test.go
+++ tfvalue_test.go
@@ -8,14 +8,44 @@ import (
 
 func TestString(t *testing.T) {
 	cases := []struct {
-		name string
+		name     string
 		ctyValue cty.Value
-		expect string
+		expect   string
 	}{
 		{
-			name: "primitive string",
+			name:     "primitive string",
 			ctyValue: cty.StringVal("value"),
-			expect: "value",
+			expect:   "value",
+		},
+		{
+			name:     "primitive number int",
+			ctyValue: cty.NumberIntVal(123),
+			expect:   "123",
+		},
+		{
+			name:     "primitive number float",
+			ctyValue: cty.NumberFloatVal(123.5),
+			expect:   "123.5",
+		},
+		{
+			name:     "primitive negative number float",
+			ctyValue: cty.NumberFloatVal(-543.21),
+			expect:   "-543.21",
+		},
+		{
+			name:     "primitive bool true",
+			ctyValue: cty.BoolVal(true),
+			expect:   "true",
+		},
+		{
+			name:     "primitive bool false",
+			ctyValue: cty.BoolVal(false),
+			expect:   "false",
+		},
+		{
+			name:     "list string",
+			ctyValue: cty.TupleVal([]cty.Value{cty.StringVal("ap-northeast-1a"), cty.StringVal("ap-northeast-1c"), cty.StringVal("ap-northeast-1d")}),
+			expect:   `["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]`,
 		},
 	}
 
