11b3c9926bf7f1802453e1c479d5618f1173dff4
thaim <thaim24@gmail.com>
2023-04-13 21:49:52 +0900

    implement to retrieve Variable api

--- cmd_diff.go
+++ cmd_diff.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"io"
+	"os"
+
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
@@ -12,5 +18,26 @@ func NewDiffOption(c *cli.Context) *DiffOption {
 }
 
 func Diff(c *cli.Context) error {
+	ctx := context.Background()
+	log.Debug().Msg("show command")
+
+	diffOpt := NewDiffOption(c)
+
+	tfeClient, err := NewTfeClient(c)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to build tfe client")
+		return err
+	}
+	organization, workspaceName = updateTerraformCloudWorkspace(organization, workspaceName, ".")
+	w, err := tfeClient.Workspaces.Read(ctx, organization, workspaceName)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to access workspace %s/%s", organization, workspaceName)
+		return err
+	}
+
+	return diff(ctx, w.ID, tfeClient.Variables, diffOpt, os.Stdout)
+}
+
+func diff(ctx context.Context, workspaceId string, tfeVariables tfe.Variables, diffOpt *DiffOption, w io.Writer) error {
 	return nil
 }
