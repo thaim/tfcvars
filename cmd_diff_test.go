bc2195b4cceac60502b98f606427a252c7194fd1
thaim <thaim24@gmail.com>
2023-05-08 22:47:41 +0900

    fix test data

--- cmd_diff_test.go
+++ cmd_diff_test.go
@@ -189,7 +189,7 @@ func TestCmdDiff(t *testing.T) {
 				mc.EXPECT().
 					List(context.TODO(), "w-test-no-vars-workspace", nil).
 					Return(&tfe.VariableList{
-						Items: []*tfe.Variable{{}},
+						Items: []*tfe.Variable{},
 					}, nil).
 					AnyTimes()
 			},
