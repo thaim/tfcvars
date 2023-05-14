7e2e750e523d85163f0bdef1b2693031b6f2baa5
thaim <thaim24@gmail.com>
2023-05-14 22:36:25 +0900

    Revert "remove unecessary variable definition"
    This reverts commit 24e7bd34db6647494da855df7fa2da46e5a60852.

--- cmd_show.go
+++ cmd_show.go
@@ -71,7 +71,9 @@ func Show(c *cli.Context) error {
 
 	showOpt := NewShowOption(c)
 	workspaceId := ""
-	var tfeClient *tfe.Client
+	var Variables tfe.Variables
+	var VariableSets tfe.VariableSets
+	var VariableSetVariables tfe.VariableSetVariables
 	if requireTfcAccess(showOpt) {
 		tfeClient, err := NewTfeClient(c)
 		if err != nil {
@@ -86,9 +88,12 @@ func Show(c *cli.Context) error {
 		}
 
 		workspaceId = w.ID
+		Variables = tfeClient.Variables
+		VariableSets = tfeClient.VariableSets
+		VariableSetVariables = tfeClient.VariableSetVariables
 	}
 
-	err := show(ctx, workspaceId, tfeClient.Variables, tfeClient.VariableSets, tfeClient.VariableSetVariables, showOpt, os.Stdout)
+	err := show(ctx, workspaceId, Variables, VariableSets, VariableSetVariables, showOpt, os.Stdout)
 	if err != nil {
 		return err
 	}
