f48567a95a853ad81deb0af10ae893e2685152e3
thaim <thaim24@gmail.com>
2023-05-23 20:43:00 +0900

    add usage to each commands

--- main.go
+++ main.go
@@ -44,26 +44,31 @@ func main() {
 		Commands: []*cli.Command{
 			{
 				Name: "help",
+				Usage: "Show this help",
 			},
 			{
 				Name:   "show",
 				Action: Show,
 				Flags:  showFlags(),
+				Usage: "Show variables on Terraform Cloud",
 			},
 			{
 				Name:   "diff",
 				Action: Diff,
 				Flags:  diffFlags(),
+				Usage: "Show difference of variables between local tfvars and Terraform Cloud variables",
 			},
 			{
 				Name:   "pull",
 				Action: Pull,
 				Flags:  pullFlags(),
+				Usage: "update local tfvars with Terraform Cloud variables",
 			},
 			{
 				Name:   "push",
 				Action: Push,
 				Flags:  pushFlags(),
+				Usage: "update Terraform Cloud variables with local tfvars",
 			},
 		},
 		Version: versionFormatter(getVersion(), getRevision()),
