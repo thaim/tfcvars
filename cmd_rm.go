a88bf26bf77c7ac80b0ddaae2d02f0b1abf0d7b0
thaim <thaim24@gmail.com>
2023-06-09 22:38:52 +0900

    add internal remove function

--- cmd_rm.go
+++ cmd_rm.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"context"
+
+	tfe "github.com/hashicorp/go-tfe"
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 )
 
+type RemoveOption struct {
+	variableKey string
+	autoApprove bool
+}
+
 func Remove(c *cli.Context) error {
+	return nil
+}
+
+func remove(ctx context.Context, workspaceId string, tfeVariables tfe.Variables, rmOpt *RemoveOption, vars *tfe.VariableList) error {
 	return nil
 }
