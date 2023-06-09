e278b0c409e7a9c66ac2ef25326522e01cf87d17
thaim <thaim24@gmail.com>
2023-06-09 22:47:15 +0900

    implement Remove cmd function

--- cmd_rm.go
+++ cmd_rm.go
@@ -13,10 +13,37 @@ type RemoveOption struct {
 	autoApprove bool
 }
 
-func Remove(c *cli.Context) error {
-	return nil
+func NewRemoveOption(c *cli.Context) *RemoveOption {
+	var opt = &RemoveOption{}
+	opt.variableKey = c.String("variable")
+	opt.autoApprove = c.Bool("auto-approve")
+
+	return opt
 }
 
-func remove(ctx context.Context, workspaceId string, tfeVariables tfe.Variables, rmOpt *RemoveOption, vars *tfe.VariableList) error {
+func Remove(c *cli.Context) error {
+	ctx := context.Background()
+	log.Debug().Msg("rm command")
+
+	tfeClient, err := NewTfeClient(c)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to build tfe client")
+		return err
+	}
+
+	organization, workspaceName := updateTerraformCloudWorkspace(organization, workspaceName, ".")
+	workspace, err := tfeClient.Workspaces.Read(ctx, organization, workspaceName)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to access workspace %s/%s", organization, workspaceName)
+		return err
+	}
+
+	rmOpt := NewRemoveOption(c)
+	log.Debug().Msgf("rmOpt: %+v", rmOpt)
+
+	return remove(ctx, workspace.ID, tfeClient.Variables, rmOpt)
+}
+
+func remove(ctx context.Context, workspaceId string, tfeVariables tfe.Variables, rmOpt *RemoveOption) error {
 	return nil
 }
